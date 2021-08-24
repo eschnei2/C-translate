5750139ace45078a5c4619e0a5e5e3bac7bf6001
eschnei2 <everettschneider@gmail.com>
2021-08-24 15:17:40 -0500

    finished one thingy

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,25 @@
-// Put your code here
-
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
-console.log("All Pace Names");
+console.log("All 'The' Place Names");
 
-for (const name of names) {
+// filter with if statement at for loop
+/* for (const name of names) {
+  if (name.startsWith("The"))
+  console.log(name);
+} */
+
+// filter with filter function before for loop
+const theFilter = names.filter((name) => name.startsWith("The"));
+
+for (const name of theFilter) {
   console.log(name);
 }
